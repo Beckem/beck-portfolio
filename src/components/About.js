8e52487c7097a40b5cefa6525eced965dc99a216
= <boychienga12@gmail.com>
2022-06-13 07:39:01 +0700

    feat: animate

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
+
 import LaptopBg from "../assets/images/LaptopBg2.png";
+import MyCV from "../assets/pdf/Ha_Phu_Quy.pdf";
+import { DownloadIcon } from "@heroicons/react/outline";
+
+import { motion, useAnimation } from "framer-motion";
+import { useInView } from "react-intersection-observer";
 
 function About() {
+  const control = useAnimation();
+  const [ref, inView] = useInView();
+  const variant = {
+    visible: {
+      opacity: 1,
+      transition: { ease: "easeOut", duration: 1.5 },
+    },
+    hidden: { opacity: 0 },
+  };
+
+  useEffect(() => {
+    if (inView) {
+      control.start("visible");
+    } else control.start("hidden");
+  }, [control, inView]);
   return (
     <div className="h-screen w-screen bg-gradient ">
       <div
@@ -10,8 +31,14 @@ function About() {
           backgroundImage: `url(${LaptopBg})`,
         }}
       >
-        <div className="absolute w-[51%] left-1/2 -translate-x-1/2 pt-[32%] top-[50%] -translate-y-1/2 -mt-[2%] overflow-y-auto">
-          <div className="absolute top-0 text-gray-300 md:text-xl text-sm space-y-4">
+        <motion.div
+          ref={ref}
+          variants={variant}
+          initial="hidden"
+          animate={control}
+          className="absolute  w-[51%] left-1/2 -translate-x-1/2 pt-[32%] top-[50%] -translate-y-1/2 -mt-[2%] overflow-y-auto"
+        >
+          <div className="absolute top-0 text-gray-300 md:text-xl text-sm space-y-4 w-full">
             <p className="text-[#ff7675]">{"{"}</p>
             <p className="ml-6">
               <span className="text-[#fdcb6e]">Name</span> :{" "}
@@ -70,8 +97,17 @@ function About() {
               <span className="text-[#fd79a8]">{"]"}</span>,
             </p>
             <p className="text-[#ff7675]">{"}"}</p>
+
+            <a
+              href={MyCV}
+              download
+              className="border md:w-[110px] w-[91px] p-2 rounded-lg flex items-center space-x-2 hover:shadow-md hover:shadow-[#ffeaa7] mx-auto hover:-translate-y-1 "
+            >
+              <p className="text-[#ffeaa7]">My CV</p>
+              <DownloadIcon className="w-6 text-green-400" />
+            </a>
           </div>
-        </div>
+        </motion.div>
       </div>
     </div>
   );
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useEffect, useRef } from "react";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import {
@@ -8,8 +8,25 @@ import {
 } from "@heroicons/react/solid";
 import emailjs from "@emailjs/browser";
 import Footer from "./Footer";
+import { motion, useAnimation } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+
 function Contact() {
   const form = useRef();
+  const control = useAnimation();
+  const [ref, inView] = useInView();
+  const variant = {
+    visible: {
+      opacity: 1,
+      y: 0,
+    },
+    hidden: { opacity: 0, y: 160 },
+  };
+  useEffect(() => {
+    if (inView) {
+      control.start("visible");
+    }
+  }, [control, inView]);
 
   const sendEmailSuccess = () =>
     toast("🦄 Message has been sent!", {
@@ -57,17 +74,35 @@ function Contact() {
 
   return (
     <div className="bg-gradient h-screen w-screen">
-      <div className="w-[70%] mx-auto py-[100px]">
-        <h2 className="text-5xl text-center font-semibold text-[#ff7675]">
+      <div ref={ref} className="w-[70%] mx-auto py-[100px]">
+        <motion.h2
+          variants={variant}
+          initial="hidden"
+          animate={control}
+          transition={{ ease: "easeOut", duration: 0.8 }}
+          className="text-5xl text-center font-semibold text-[#ff7675]"
+        >
           Get In Touch
-        </h2>
-        <p className="text-center mt-4 text-lg text-white drop-shadow">
+        </motion.h2>
+        <motion.p
+          variants={variant}
+          initial="hidden"
+          animate={control}
+          transition={{ ease: "easeOut", duration: 0.6, delay: 0.8 }}
+          className="text-center mt-4 text-lg text-white drop-shadow"
+        >
           My inbox is always open. Whether you have a question or just want to
           say hi, I'll try my best to get back to you!
-        </p>
+        </motion.p>
 
         <div className="flex mt-12">
-          <div className="flex flex-col space-y-16 text-white flex-[2]">
+          <motion.div
+            variants={variant}
+            initial="hidden"
+            animate={control}
+            transition={{ ease: "easeOut", duration: 0.6, delay: 1.4 }}
+            className="flex flex-col space-y-16 text-white flex-[2]"
+          >
             <div className="flex items-center space-x-6">
               <div className="h-16 w-16 rounded-full bg-green-400 relative">
                 <LocationMarkerIcon className="h-8 w-8 absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2" />
@@ -86,9 +121,13 @@ function Contact() {
               </div>
               <div className="text-lg ">+84-889912777</div>
             </div>
-          </div>
+          </motion.div>
 
-          <form
+          <motion.form
+            variants={variant}
+            initial="hidden"
+            animate={control}
+            transition={{ ease: "easeOut", duration: 0.6, delay: 2 }}
             ref={form}
             className="flex flex-col flex-[3] space-y-8"
             onSubmit={sendEmail}
@@ -126,7 +165,7 @@ function Contact() {
               value="Send"
               className="bg-green-400 w-max px-10 py-3 text-lg rounded-full text-white cursor-pointer hover:bg-green-600"
             />
-          </form>
+          </motion.form>
         </div>
       </div>
       <ToastContainer />
